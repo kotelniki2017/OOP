9d98b3cdce418503ae84af4992ac3f4b83c4818e
junaart <junaart@mail.ru>
2018-10-03 12:11:31 +0300

    question_1_11

---
+++ Code/Qustion_1_11.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+struct two_list{
+  int data;
+  struct two_list *next;
+  struct two_list *prev;
+};
+
+void add_first(two_list * &p,two_list * &tail, int a)
+{
+  two_list *current = new two_list;
+  if (p != NULL)
+    {
+      current->data = a;
+      current->next = p;
+      current->prev = NULL;
+      p->prev = current;
+      p=current;
+    }
+  else
+    {
+      current->data = a;
+      current->next = p;
+      current->prev = NULL;
+      p=current;
+      tail = p;
+    }
+}
+
+void add_last(two_list *&p1, two_list *&p2, int a)
+{
+  two_list *current=new two_list;
+  if(p2!=NULL){
+    current->data=a;
+    current->prev=p2;
+    current->next=NULL;
+    p2->next=current;
+    p2=current;
+  }
+  else
+    {
+      current->data=a;
+      current->prev=p2;
+      current->next=NULL;
+      p2=current;
+      p1=p2;
+    }
+  
+
+
+int main()
+{
+  two_list *first = NULL, *last = NULL;
+  
+  return 0;
+}
